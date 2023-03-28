ac04bd99fc77c038e582600bad402b52d3b25f76
baranpoolat <baranpolat96@gmail.com>
2023-03-27 22:00:30 -0400

    stock-app first part completed

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import SideMenu from "./SideMenu";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <SideMenu />
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
